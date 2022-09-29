81d7c19cc8f90af696ef5f85e87e9838309fd007
JonathanValleA <jvalle@jaumebalmes.net>
2022-09-29 19:15:15 +0200

    Actividad 3

---
+++ vehicle.php
@@ -0,0 +1,22 @@
+<?php
+    private $nom;
+    private $model;
+    private $marca;
+    private $cavalls;
+    private $añoV;
+
+    function __construct(){
+
+    }
+    function __construct1($nom, $model, $marca, $cavalls, $añoV){
+        $this->nom = $nom;
+        $this->model = $model;
+        $this->marca = $marca;
+        $this->cavalls = $cavalls;
+        $this->añoV = $añoV;
+    }
+    function __condtruct2($model, $marca){
+        $this->model = $model;
+        $this->marca = $marca;
+    }
+?>
